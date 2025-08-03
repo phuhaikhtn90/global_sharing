9ab9a3f6f910c783dc4d06b42c0bfff722c4fa9f
Hai Le <>
2025-08-04 06:48:25 +0700

    - update expected output

--- 2025_07_31_get_figma_API/extract_figma_component_tokens.py
+++ 2025_07_31_get_figma_API/extract_figma_component_tokens.py
@@ -6,8 +6,8 @@ headers = {
     "X-Figma-Token": TOKEN
 }
 
-file_key = "6dCATf55Qdby8cP0NfcY6n"
-node_id = "1-5055"
+file_key = "lciV4feePMcVoQA0Bywfzz"
+node_id = "2-96"
 
 url = f"https://api.figma.com/v1/files/{file_key}/nodes?ids={node_id}"
 resp = requests.get(url, headers=headers)
